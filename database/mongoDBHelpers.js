7c51188d9d0f5b791a32868121de32ef23fbb53b
Jennifer Haren <jennifer.haren110@gmail.com>
2019-05-10 16:08:40 -0700

    Exercise 3 complete
    Implement mongoDBHelpers that use mongoose methods .create, .find and .findOneAndUpdate to query the TodoList collection.

---
+++ database/mongoDBHelpers.js
@@ -0,0 +1,20 @@
+const { TodoList } = require('./models.js');
+
+const createTodoHelper = (todo, listName) => {
+  return TodoList.create({ todo, listName })
+};
+
+const getAllTodosHelper = () => {
+  return TodoList.find({});
+};
+
+const updateTodoHelper = (id, newTodo) => {
+  console.log(id, newTodo);
+  return TodoList.findOneAndUpdate({ _id: id }, {todo: newTodo});
+};
+
+module.exports = {
+  createTodoHelper,
+  getAllTodosHelper,
+  updateTodoHelper
+}
